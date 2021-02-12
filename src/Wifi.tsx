358a900c076400901e75c96222e13cd056a4da9c
Maxwel Dsouza <maxweldsouza.in@gmail.com>
2021-02-12 16:46:16 +0530

    Cleanup, fix wifi

--- src/Wifi.tsx
+++ src/Wifi.tsx
@@ -1,25 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Wifi } from 'react-feather';
-import { Progress, DiskProgressInner, Value } from './styledComponents';
 // import Select from 'react-select';
 import { applyWifi, getWifi } from './wifi';
-import { useInterval } from 'react-use';
-import useClickAway from 'react-use/lib/useClickAway';
-import chroma from 'chroma-js';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
+// import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 
 function WifiSelect() {
   const [wifiNetworks, setWifiNetworks] = useState([]);
-  const [value, setValue] = useState<{ value: string; label: string }>('');
+  const [value, setValue] = useState<string>('');
 
   useEffect(() => {
     getWifi().then((w) => {
       setWifiNetworks(w);
-      const selected = w.filter((wifi: { [x: string]: string; }) => wifi?.['active'] === '*')?.[0];
-      setValue(selected);
+      const selected = w.filter((wifi: { [x: string]: string; }) => wifi?.['active'] === 'yes')?.[0];
+      setValue(selected.name);
     });
   }, []);
 
@@ -35,16 +31,14 @@ function WifiSelect() {
       <Wifi />
       Wifi
       <FormControl>
-        <InputLabel>Select a wifi connection...</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           value={value}
           onChange={(e) => {
-            const temp = options.filter(
-              (option) => option.value === e.value
-            )?.[0];
-            setValue(temp);
-            applyWifi(e.value);
+            const value : string = e.target.value;
+            applyWifi(value).then(() => {
+              setValue(value);
+            });
           }}
           variant={'standard'}
           label={'Select a wifi connection...'}
